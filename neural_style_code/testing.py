76c3fc019ff9cc209bd465e419dffddf9c66c365
Christopher Cole <48872423+DirtyTechSmith@users.noreply.github.com>
2019-12-31 19:38:48 -0800

    stiff

---
+++ neural_style_code/testing.py
@@ -0,0 +1,10 @@
+import os
+import sys
+source_path = os.path.dirname(__file__)
+source_path = os.path.dirname(source_path)
+images_root = source_path + '/images/'
+output_dir = images_root + 'output/'
+sources_dir = images_root + 'source_images/'
+styles_dir = images_root + 'style_images/'
+
+
